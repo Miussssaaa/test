611e807e9a79281df8c12c5348070fee5325b1fb
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-01 14:34:39 +0900

    文字列の複数回表示メソッド

--- src/TypeSafe/Main.java
+++ src/TypeSafe/Main.java
@@ -1,9 +1,20 @@
 package TypeSafe;
 
 public class Main {
+	//第一引数の文字列を第二引数の数字分だけ出力する
+	//第一引数 : 表示したい文字
+	//第二引数 : 表示したい回数
+
+	public static void prints(String a, int b) {
+
+		for(int i = 0; i<b; i++) {
+			System.out.println(a);
+		}
+
+	}
 
 	public static void main(String[] args) {
-		//第一引数の文字列を第二引数の数字分だけ出力する
+
 
 	}
 
