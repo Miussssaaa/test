b5e83ac86d8f44c03a39fde44d3c6d56e5fc16ae
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-08 14:41:08 +0900

    Accountクラスにフィールド変数追加 カプセル化に伴う処理実装

--- src/Enum/Account.java
+++ src/Enum/Account.java
@@ -2,4 +2,32 @@ package Enum;
 
 public class Account {
 
+	private String accountNum; //口座番号
+	private int zandaka; //残高
+	private String accountType; //口座の種類
+
+
+	//setter
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+	public void setZandaka(int zandaka) {
+		this.zandaka = zandaka;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	//getter
+	public String getAccountNum() {
+		return this.accountNum;
+	}
+	public int getZandaka() {
+		return this.zandaka;
+	}
+	public String getAccountType() {
+		return this.accountType;
+	}
+
+
 }
