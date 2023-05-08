590856d3a6f182db71c4b0bd636951e2594a6c3b
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-08 13:29:45 +0900

    キャスト部分を修正

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Pocket p = new Pocket();
+		Pocket<String> p = new Pocket();
 
 		//データの入力
 		p.put("1111");
 
 		//データの取り出し
-		String s = (String)p.get();
+		String s = p.get();
 
 		//表示
 		System.out.println(s);
