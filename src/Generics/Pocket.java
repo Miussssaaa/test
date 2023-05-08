d43d41830f67acde24f309424024237ca36f9fb1
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-08 11:54:25 +0900

    データ格納用変数追加

--- src/Generics/Pocket.java
+++ src/Generics/Pocket.java
@@ -2,4 +2,14 @@ package Generics;
 
 public class Pocket {
 
+	//データ格納用変数
+	private Object data;
+
+	public void put(Object d) {
+		this.data = d;
+	}
+
+	public Object get() {
+		return this.data;
+	}
 }
