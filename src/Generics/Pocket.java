9e7266e6cd7b1a4a7cdc56cdf6080a7e456d429c
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-08 12:27:15 +0900

    ジェネリクス(仮型引数)を設定

--- src/Generics/Pocket.java
+++ src/Generics/Pocket.java
@@ -1,15 +1,23 @@
 package Generics;
 
-public class Pocket {
+import java.util.ArrayList;
+
+//				    ↓ジェネリクス(仮型引数)　一度型を入れるとその型以外入れられなくなる
+//				  　  <>に入れた型に全部変換される
+public class Pocket<E> {
 
 	//データ格納用変数
-	private Object data;
+	private E data;
 
-	public void put(Object d) {
+	//代入するときのメソッド
+	public void put(E d) {
 		this.data = d;
 	}
 
-	public Object get() {
+	//値を取得するときのメソッド
+	public E get() {
 		return this.data;
 	}
+
+	ArrayList<E> list = new ArrayList();
 }
