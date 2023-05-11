73cd477059c2e902b59aed5544aa14c022f6cab3
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-11 13:43:08 +0900

    decorateメソッドを追加

--- src/NullSafe/Main.java
+++ src/NullSafe/Main.java
@@ -4,9 +4,36 @@ package NullSafe;
 import java.util.Optional;
 
 public class Main {
+
+/*	 decorateメソッド
+	 目的：文字列の両端に文字を追加する（$たなか$）
+	 第一引数：文字列（たなか）
+	 第二引数：はさむ文字（$）
+	 戻り値：Optional<String>　nullが格納される可能性があることを考慮した処理を呼び出し側に強制させる
+	 処理：文字列がnull、もしくは文字数が0の場合nullを返す。
+	 正しい引数が設定された場合、文字列の両端にはさむ文字を追加する
+*/
+
+	public static Optional<String> decorate(String s, char c){
+
+		String str;
+
+		//文字列がnullでないか、0文字じゃないかを判定
+		if(s == null || s.length()<=0) {
+			str = null; //0だった場合strにnullを格納
+		}else {
+			str = c + s + c; //文字列の両端に文字を挟みstrに代入
+		}
+		return Optional.ofNullable(str); //ofNullableを使うことでNullPointerエラーを回避、return
+	}
+
+
+
+
+
 	public static void main(String[] args) {
 
-		//nullが入っているかチェックしながら代入を行う(newで生成不可)
+		//nullが入っているかチェックしながらインスタンス生成を行う(newで生成不可)
 
 		//Nullableはnullの代入を許可する
 		Optional<String> op1 = Optional.ofNullable("タナカ");
@@ -29,6 +56,11 @@ public class Main {
 		// orElse() は中身がnullだった場合引数を表示する
 		System.out.println("名前 : " + op1.orElse("ななし"));
 		System.out.println("名前 : " + op2.orElse("ななし"));
+
+
+		Optional<String> op3 = decorate("たな",'$');
+		System.out.println(op3.orElse("nullが格納されています"));
+
 
 	}
 
