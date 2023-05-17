fec35aece0d22d357552bd4f9d117310ea5391ea
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-05-17 09:45:53 +0900

    Function型,Consumer型など定義

--- src/MethodObject/Main.java
+++ src/MethodObject/Main.java
@@ -1,6 +1,9 @@
 package MethodObject;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
 
@@ -19,10 +22,36 @@ public class Main {
 
 	public static void main(String[] args) {
 
+
 		//Functionは様々な処理を1つにまとめて、名前をつけることができるもの
 		//関数を引数に代入
 		Function<String,Integer>func = Main::len;
-//				↑引数  戻り値		   クラス名::メソッド名
+		//		 ↑引数  戻り値		   クラス名::メソッド名
+
+
+		//戻り値なしver
+		//Consumerは消費者という意味 中にsysoutが格納されている
+		Consumer<String>cons = System.out::println;
+
+		//引数を受け取るだけで返す値はない
+		cons.accept("特級オブジェクト");
+
+
+		//引数なしver
+		//Supplierは提供者という意味 中に改行コードを格納
+		Supplier<String>sup = System::lineSeparator;
+
+		//引数を受け取らず改行コードのみ出力
+		cons.accept("一行目" + sup.get() + "二行目");
+
+
+		//引数2つver(システム情報表示)
+		//BiFunction<引数,引数,戻り値>
+		BiFunction<String,String,String> bFunc = System::getProperty;
+
+		cons.accept(bFunc.apply("java.version", "keyが正しくありません"));
+
+
 
 		//文字数格納用変数
 		int length = func.apply("特級オブジェクト");
