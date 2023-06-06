36ab7ae8ce0d2ac8c1c14c67758afdf882ec8718
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-06-06 14:21:03 +0900

    streamを使用した処理に変更

--- src/Stream/Main.java
+++ src/Stream/Main.java
@@ -2,6 +2,7 @@ package Stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,7 +11,7 @@ public class Main {
 		//Heroを20人格納するリストを作成
 		List<Hero> list = new ArrayList();
 
-		for(int i = 0; i < 20; i++) {
+		for(int i = 0; i <= 20; i++) {
 			//リストにインスタンス生成したHero追加
 			list.add(new Hero("ななし" + i + "号"));
 		}
@@ -24,7 +25,27 @@ public class Main {
 
 		//②Heroのうち一人でもHP0がいたらtrueになるboolean
 
-		boolean isKnockOut = false; //HP0がいるか
+		//StreamとanyMatchを使うと一行で書ける
+		//配列やList、Collectionなどのオブジェクト名.stream().メソッド名((引数) -> {処理})
+		//「Stream API」とは配列やList、Collectionなどの要素の集合に対して処理を行う
+		//anyMatch 引数に記述したラムダ式に対してtrue か falseを返却する
+		boolean isKnockOut = list.stream().anyMatch(h -> h.getHP() == 0);
+		System.out.println(isKnockOut);
+
+		//StreamAPIの初期化(インスタンス生成のようなもの)
+		//ジェネリクスは同じ型を使用する
+		Stream<Hero> st = list.stream();
+
+
+		//繰り返し
+		list.forEach(h ->{
+
+			System.out.println("勇者の名前 : " + h.getName());
+
+		});
+
+
+/*		boolean isKnockOut = false; //HP0がいるか
 		String KnockOutHero = "不在"; //HP0の名前
 
 
@@ -41,6 +62,7 @@ public class Main {
 
 		System.out.println("倒れたヒーローの有無 : " + isKnockOut);
 		System.out.println("倒れたヒーロー : " + KnockOutHero);
+*/
 
 
 	}
