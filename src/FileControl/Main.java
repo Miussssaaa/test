00dea3692abb705aa31584e084d67e756806dba5
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-07-03 14:42:41 +0900

    write,flushを記述 コメント追記

--- src/FileControl/Main.java
+++ src/FileControl/Main.java
@@ -7,14 +7,20 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 
+		//ファイルを開くのは編集権限を持つという解釈でよい
+		//開く、閉じるの一連の動作はニュース番組の生中継(違う地点から)を想像すると良い
+
 		//ファイルを開く準備
 		FileWriter fw = new FileWriter("writeTest.dat", true);
 
-		//書き込み(依頼)
+		//書き込み(依頼) 機械がキリの良いタイミングで書き込む、そのため時間が不定
 		fw.write('A');
 
+		//書き込み(今すぐ) ↑と異なり今すぐ書き込むように命令できる
+		fw.flush();
 
-		//ファイルを開くのは編集権限をもつという解釈
+		//ファイルを閉じる
+		fw.close();
 
 
 
