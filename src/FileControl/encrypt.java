a61525118db50b150d6eefd122eb85a3fa213034
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-07-06 09:08:23 +0900

    encryptクラス作成

---
+++ src/FileControl/encrypt.java
@@ -0,0 +1,17 @@
+package FileControl;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+public class encrypt {
+	public static void main(String[]args) throws IOException{
+
+		String msg = "吾輩は猫である";
+
+		//文字ストリーム読み込み
+		Reader rd = new StringReader(msg);
+
+
+	}
+}
