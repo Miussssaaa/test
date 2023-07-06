253f4c8a234f46823e25e79ae3527929797e059c
D3\PC_D3 <piichandori220@docomo.ne.jp>
2023-07-06 10:04:44 +0900

    StringReader,ByteArrayOutputStream

--- src/FileControl/encrypt.java
+++ src/FileControl/encrypt.java
@@ -1,5 +1,6 @@
 package FileControl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
@@ -10,7 +11,21 @@ public class encrypt {
 		String msg = "吾輩は猫である";
 
 		//文字ストリーム読み込み
-		Reader rd = new StringReader(msg);
+		Reader r = new StringReader(msg);
+		System.out.println((char)r.read());
+		System.out.println((char)r.read());
+		System.out.println((char)r.read());
+
+		//バイトストリーム読み込み
+		ByteArrayOutputStream bs = new ByteArrayOutputStream();
+		bs.write(65);
+		bs.write(66);
+
+		byte[] array = bs.toByteArray();
+
+		for(byte b : array) {
+			System.out.println((char)b);
+		}
 
 
 	}
